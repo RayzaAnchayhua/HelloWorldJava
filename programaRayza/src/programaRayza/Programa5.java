27cdfecc47e28aeaa7a0a12ceb5aa3b84763f9a7
Rayza Luana <93015921+RayzaAnchayhua@users.noreply.github.com>
2023-09-24 10:22:14 -0300

    continuação dos exercicios

--- programaRayza/src/programaRayza/Programa5.java
+++ programaRayza/src/programaRayza/Programa5.java
@@ -1,18 +1,32 @@
 package programaRayza;
 
+import java.util.Scanner;
+
 public class Programa5 {
 
 	public static void main(String[] args) {
 		
 		//Entrada de dados
-	
+		Scanner teclado = new Scanner(System.in);
+		
+		double prestacao;
+		double valor;
+		double taxa;
+		double tempo;
+		
+		System.out.println("Este programa calcula o valor de uma prestação em atraso");
+		System.out.println("Insira o valor da prestação: ");
+		valor = teclado.nextDouble();
+		System.out.println("Insira o valor da taxa: ");
+		taxa = teclado.nextDouble();
+		System.out.println("Insira o valor do tempo: ");
+		tempo = teclado.nextDouble();
 		
 		//Interpretação de dados
-		
-		
+		prestacao = valor + ( valor * taxa/100)*tempo;
 		
 		//Saída de dados
+		System.out.println("Este é o valor da prestação" + prestacao);
 		
-	
 	}
 }
--- programaRayza/src/programaRayza/Programa6.java
+++ programaRayza/src/programaRayza/Programa6.java
@@ -1,18 +1,30 @@
 package programaRayza;
 
-public class Programa6 {
+import java.util.Scanner;
+
+public class Programa5 {
 
 	public static void main(String[] args) {
 		
 		//Entrada de dados
-	
+		Scanner teclado = new Scanner(System.in);
+		
+		double prestacao;
+		double valor;
+		double taxa = 7.9 ;
+		int tempo ;
+		
+		System.out.println("Este programa calcula o valor de uma prestação em atraso");
+		System.out.println("Insira o valor da prestação: ");
+		valor = teclado.nextDouble();
+		System.out.println("Insira o valor do tempo em meses: ");
+		tempo = teclado.nextDouble();
 		
 		//Interpretação de dados
-		
-		
+		prestacao = valor + ( valor * taxa/100)*tempo;
 		
 		//Saída de dados
+		System.out.println("Este é o valor da prestação" + prestacao);
 		
-	
 	}
 }
