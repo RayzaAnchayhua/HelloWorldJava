50788349566e0be7be1522883d7193825ceb0804
RayzaAnchayhua <Rayzaanchayhua@gmail.com>
2023-11-01 10:04:12 -0300

    Update

--- pancadao/src/main/java/com/mycompany/pancadao/variavel/Eleicao.java
+++ pancadao/src/main/java/com/mycompany/pancadao/variavel/Eleicao.java
@@ -18,8 +18,6 @@ public class Eleicao {
         c = (Math.random()*(votosTotais-a-b-1))+1;
         nulos = (Math.random()*(votosTotais-a-b-1))+1;
         brancos = (Math.random()* (votosTotais-a-b-c-1))+1;
-        
-        //Ta funcionando ;)
         a = (a/votosTotais)*100;
         b = (b/votosTotais)*100;
         c = (c/votosTotais)*100;
@@ -27,21 +25,21 @@ public class Eleicao {
         brancos = (brancos/votosTotais)*100;
         
         //Saida de dados
-        
+        System.out.println("Votos com porcentagem decimal");
         System.out.println("Foram registrados "+votosTotais+" votos");
         System.out.println("Porcentagem de votos do candidato A: " + a + "%");
+        System.out.println("Porcentagem de votos do candidato B: " + b + "%");
+        System.out.println("Porcentagem de votos do candidato C: " + c + "%");
+        System.out.println("Porcentagem de votos nulos: " + nulos + "%");
+        System.out.println("Porcentagem de votos brancos: " + brancos + "%\n");
         
+        System.out.println("Votos com porcentagem arredondada");
+        System.out.println("Foram registrados "+votosTotais+" votos");
         System.out.printf("Porcentagem de votos do candidato A: %4.2f %%\n", a);
         System.out.printf("Porcentagem de votos do candidato A: %.0f %%\n", a);//Aprendi isso
-        
-        System.out.println("Porcentagem de votos do candidato B: " + b + "%");
         System.out.printf("Porcentagem de votos do candidato B: %.0f%%\n", b);
-        System.out.println("Porcentagem de votos do candidato C: " + c + "%");
         System.out.printf("Porcentagem de votos do candidato C: %.0f%%\n", c);
-        System.out.println("Porcentagem de votos nulos: " + nulos + "%");
         System.out.printf("Porcentagem de votos nulos: %.0f%%\n", nulos);
-        System.out.println("Porcentagem de votos brancos: " + brancos + "%");
         System.out.printf("Porcentagem de votos brancos: %.0f%%\n", brancos);
-        
     }
 }
