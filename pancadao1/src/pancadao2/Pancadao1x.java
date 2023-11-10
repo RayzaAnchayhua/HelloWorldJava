03cd3e8b81b3a76be2caed5da353e7762fe2bb84
Rayza Luana <93015921+RayzaAnchayhua@users.noreply.github.com>
2023-11-09 21:57:27 -0300

    update exercicios

--- pancadao1/src/pancadao2/Pancadao1x.java
+++ pancadao1/src/pancadao2/Pancadao1x.java
@@ -1,8 +1,19 @@
 package pancadao2;
 
+import java.util.Scanner;
+
 public class Pancadao1x {
     public static void main(String[] args) {
         //Entrada de dados
+        double base;
+        double indice;
+        double calculo;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Informe um valor para a base da raiz: ");
+        base = teclado.nextDouble();
+
+        //Processamento de dados
+        
 
     }
 }
